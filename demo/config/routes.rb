cc6c3e39ed0d63f85c66f74a6be77a940d5051f9
wiserkuo <wiserkuo@wiserkuos-MacBook-Pro.local>
2015-05-22 00:42:58 +0800

    Ruby Time.now , link_to , root_path , welcome_say_hello_path

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -54,4 +54,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
       get "welcome/say_hello" => "welcome#say"
+      get "welcome" => "welcome#index"
+      root :to => "welcome#index"
 end
