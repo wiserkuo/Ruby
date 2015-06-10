8e6ba67892be86bb5bc0730f8a5261dc7f460da1
wiserkuo <wiserkuo@wiserkuos-MacBook-Pro.local>
2015-06-11 00:17:16 +0800

    ruby study 1st

---
+++ hello.rb
@@ -0,0 +1,27 @@
+puts "Hello, World!!"
+puts 1.0+2.0
+puts 2.0*3.0
+puts 5.0-8.0
+puts 9.0/2.0
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts 5*(12-8)+-15
+puts 98+(59872/(13*8))*-52
+puts 'I like '+'apple pie'
+puts 'You\'re smart!'
+
+var1 = 'stop'
+var2 = 'foobar'
+var3 = "aAbBcC"
+
+puts var1.reverse 
+puts var2.length
+puts var3.upcase
+puts var3.downcase
+
+verb ='work'
+where ='office'
+
